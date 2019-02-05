5dde900f6a5e0b42dfc2898504b31339a54f4081
hackerman-vivek <vki.rana456@gmail.com>
2019-02-06 02:54:57 +0530

    hello

---
+++ config/prod-database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    mongoDbUrl: process.env.MONGO_DB_URI
+
+};
---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+
+const CommentSchema = new Schema({
+
+
+    user: {
+
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+
+
+    },
+
+
+
+
+    body:{
+
+        type: String,
+        required: true
+
+    },
+
+
+    approveComment:{
+
+        type: Boolean
+
+
+    },
+
+
+
+    date: {
+
+        type: Date,
+        default: Date.now()
+
+
+    }
+
+});
+
+module.exports = mongoose.model('comments', CommentSchema);
